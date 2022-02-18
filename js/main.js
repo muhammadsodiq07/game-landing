b2ad7ec2516e1d36552c9f645db503415ff4b7a7
muhammadsodiq07 <muhammadsodiqkoziyev1@gmail.com>
2022-02-18 22:44:25 +0300

    added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ elBtn.addEventListener('click', function(){
   elNav.classList.toggle('show');
   elBtnToggle.classList.toggle('active')
 })
+
+
+
+//Get the button
+var topbutton = document.getElementById("id-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topbutton.style.opacity = "1";
+    topbutton.style.transition = "all 0.5s linner";
+  } else {
+    topbutton.style.opacity = "0";
+    topbutton.style.transition = "all 0.5s linner";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
